d5fcf6a8c747594b8ee8aaa1f8145e06d527f79a
Harsh Patel <159167245+harsh4418@users.noreply.github.com>
2026-04-18 12:45:20 +0530

    Delete tests/classwork directory

--- tests/classwork/dropdowns.spec.js
+++
@@ -1,6 +0,0 @@
-import {test} from "@playwright/test"
-
-test("dropdown", async({page})=>{
-    await page.goto("https://demoapps.qspiders.com/ui/dropdown?sublist=0");
-    await page.locator('//select[@id="country_code"]').selectOption({value:"+92"});
-})
--- tests/classwork/first.spec.js
+++
@@ -1,4 +0,0 @@
-import {test} from "@playwright/test";
-// test("1st example", async()=>{
-//     console.log("hi");
-// })
